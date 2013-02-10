239fcf839bce068029473aa357a67e4032ca82ed
TacoSteemers <tsteemers@gmail.com>
2013-02-10 11:34:48 +0100

    usb.h: Added header guard

--- code/usb.h
+++ code/usb.h
@@ -1,5 +1,10 @@
+#ifndef USB_H
+#define USB_H
+
 void doCheck(void);
 void processItem(char *str);
 void getDeviceInfo(char* out, int *pOutLen, const char device[]);
 void addDeviceInfo(char *id, int *p_len, const char *source, const char *property);
 int checkIfItemMounted(char *str);
+
+#endif
