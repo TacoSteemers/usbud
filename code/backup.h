efd3d3800755784d64f04c45d32357523e7b1237
TacoSteemers <tsteemers@gmail.com>
2013-03-02 22:20:15 +0100

    backup.h: missing header guard

--- code/backup.h
+++ code/backup.h
@@ -3,6 +3,11 @@
    the GNU General Public License. A version of this license should have been 
    provided. It can also be found on: http://www.gnu.org/licenses/gpl-3.0.txt */
 
+#ifndef BACKUP_H
+#define BACKUP_H
+
 void doBackup  (char *source,     char *target);
 void processDevice(char *mountPoint, char *deviceId);
 void backDeviceUp (char *mountPoint, char *deviceId);
+
+#endif
